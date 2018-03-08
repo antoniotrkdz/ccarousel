b37d162ab9384568a1f0bb6800a05985a7a204fd
antoniotrkdz <antoniotrkdz@gmail.com>
2018-03-08 01:25:34 +0100

    Add random image from unsplash.it Makes an array so you can load several random images as opposed to the ?random parameter that gives the same image multiple times

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.fetchImages = this.fetchImages.bind(this);
+
+    this.state = {
+      images: [],
+    };
+  }
+
+  fetchImages() {
+    const url = new URL('https://unsplash.it/list');
+
+    fetch(url)
+      .then(response => response.json())
+      .then(response =>
+        this.setState({
+          images: response
+        })
+      )
+      .then(log => console.log('image url', this.state))
+      .catch(error => console.log('Fetch operation failed: ' + error.message));
+  }
+
+  componentWillMount() {
+    this.fetchImages();
+  }
+
   render() {
+    const randomImage = this.state.images 
+      ? Math.floor(Math.random() * this.state.images.length)
+      : null;
+    console.log("RI",randomImage)
     return (
       <div className="App">
         <header className="App-header">
           <div className="App-header-images">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">+</h1>
-            <img src="https://avatars3.githubusercontent.com/u/18146764?s=400&v=4" className="App-logo-nospin" alt="logo" />
+            <img
+              src="https://avatars3.githubusercontent.com/u/18146764?s=400&v=4"
+              className="App-logo-nospin"
+              alt="logo"
+            />
           </div>
-          <h1 className="App-title">Welcome to<br/>
-            Antonio Trabalza skills test for Cantiere Creativo</h1>
+          <h1 className="App-title">
+            Welcome to<br />
+            Antonio Trabalza skills test for Cantiere Creativo
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <img src={"https://unsplash.it/300?image=" + randomImage} alt="" />
+        </div>
       </div>
     );
   }
