98d721ab57ae7e9c4a89f18f03c98eafc7096504
antoniotrkdz <antoniotrkdz@gmail.com>
2018-03-11 15:58:32 +0100

    Using HashRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
